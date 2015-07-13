e442186f64891951869c197b3ae10925338fddbf
Joao Vitor Pietsiaki Moraes <joao.moraes@lactec.org.br>
2015-07-13 15:34:36 -0300

    Created MonoGame projects for XTiled and XTiledExtensions.

--- XTiled/Properties/AssemblyInfo.cs
+++ XTiled/Properties/AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+#if !PORTABLE
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type. Only Windows
@@ -23,6 +24,7 @@ using System.Runtime.InteropServices;
 // project is exposed to COM. On other platforms, it unique identifies the
 // title storage container when deploying this assembly to the device.
 [assembly: Guid("8205c62d-57d6-41b2-95b5-c4407fc76366")]
+#endif
 
 // Version information for an assembly consists of the following four values:
 //
